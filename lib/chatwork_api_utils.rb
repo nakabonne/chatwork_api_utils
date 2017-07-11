e4c285774961521a5780b4301063c751d8a11a14
ryonakao <rodriguez.nak@gmail.com>
2017-07-11 17:27:40 +0900

    add method notify

--- lib/chatwork_api_utils.rb
+++ lib/chatwork_api_utils.rb
@@ -1,5 +1,12 @@
 require "chatwork_api_utils/version"
+require 'net/https'
+require 'json'
 
 module ChatworkApiUtils
-  # Your code goes here...
+  def self.notify(room_id ,message, api_token)
+    uri = URI.parse('https://api.chatwork.com')
+    client = Net::HTTP.new(uri.host, 443)
+    client.use_ssl = true
+    response = client.post("/v2/rooms/#{room_id}/messages", "body=#{message}", {'X-ChatWorkToken' => api_token})
+  end
 end
